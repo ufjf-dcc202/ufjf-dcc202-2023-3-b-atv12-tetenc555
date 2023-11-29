d54a3b1fd82032c1dd6600a5be824cf03950feee
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-28 22:37:36 -0300

    cria destino

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 document.entrada.addEventListener('submit',leFormulario);
+const olJoao = document.querySelector("ol#joao");
+const olMaria = document.querySelector("ol#maria");
 
 
 function leFormulario(event){
     event.preventDefault();
-    const quantidade= document.entrada.quantidade.valueAsNumber;
+    const quantidade= Number(document.entrada.quantidade);
     const fruta= document.entrada.fruta.value;
     const origem=document.entrada.origem.value;
     const destino=document.entrada.destino.value;
