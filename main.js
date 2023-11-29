cd7317eb65562946cf99f793b26fab53db0fd9b4
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-28 22:28:47 -0300

    corrige nome da variavel

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function leFormulario(event){
     event.preventDefault();
     const quantidade= document.entrada.quantidade.value;
     const fruta= document.entrada.fruta.value;
-    const pessoa=document.entrada.origem.value;
+    const origem=document.entrada.origem.value;
 
-    console.log(`${pessoa} tem ${quantidade} ${fruta}`);
+    console.log(`${origem} tem ${quantidade} ${fruta}`);
 }
