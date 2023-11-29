3ae2949e00289cff86f6f7359737ead034b3d4e0
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-28 22:33:00 -0300

    corrige conversao para numero

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.entrada.addEventListener('submit',leFormulario);
 
 function leFormulario(event){
     event.preventDefault();
-    const quantidade= document.entrada.quantidade.value;
+    const quantidade= document.entrada.quantidade.valueAsNumber;
     const fruta= document.entrada.fruta.value;
     const origem=document.entrada.origem.value;
     const destino=document.entrada.destino.value;
