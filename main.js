4dbc512eed73449347e5ea5e5160a84667820447
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-28 22:45:30 -0300

    corrige erros em alguns ids

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const olMaria = document.querySelector("ol#maria");
 
 function leFormulario(event){
     event.preventDefault();
-    const quantidade= Number(document.entrada.quantidade);
+    const quantidade= document.entrada.quantidade.valueAsNumber;
     const fruta= document.entrada.fruta.value;
     const origem=document.entrada.origem.value;
     const destino=document.entrada.destino.value;
