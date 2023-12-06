35c4af798a9b0259d79a9e77acc7bd44ef52ec1a
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-05 23:02:05 -0300

    otimiza funcao de transacao

--- estoque.js
+++ estoque.js
@@ -9,23 +9,72 @@ let estoque ={
 ],
 };
 
-function getEstoque(){
+function getEstoque() {
     return structuredClone(estoque);
 }
 
-function transacaoNoEstoque(origem,destino,tipo,quantidade)
-{
-    if(destino==='pomar')
-    {
-        const pessoa =estoque[origem]
-        for (let i=0;i<estoque[origem].length;i++)
-        {
-            const monte = pessoa[i];
-            if(monte.tipo === tipo)
-            {
-                monte.qtd -= Math.min(quantidade,monte.qtd);
-            }
-        }    
+function transacaoNoEstoque(origem, destino, tipo, quantidade) {
+    if (origem === destino) {
+        return;
+    }
+    if (destino === 'pomar') {
+        dePessoaParaPomar(origem, tipo, quantidade);
+        return;
+    }
+    if (origem === 'pomar') {
+        dePomarParaPessoa(destino, tipo, quantidade);
+        return;
+    }
+    const pessoaOrigem = estoque[origem];
+    const pessoaDestino = estoque[destino];
+    let monteOrigem;
+    for (let i = 0; i < pessoaOrigem.length; i++) {
+        const monte = pessoaOrigem[i];
+        if (monte.tipo === tipo) {
+            monteOrigem = monte;
+            break;
+        }
+    }
+    if (!monteOrigem) {
+        return;
+    }
+    let monteDestino;
+    for (let i = 0; i < pessoaDestino.length; i++) {
+        const monte = pessoaDestino[i];
+        if (monte.tipo === tipo) {
+            monteDestino = monte;
+            return;
+        }
+    }
+    if (!monteDestino) {
+        monteDestino = { 'tipo': tipo, 'qtd': 0 };
+        pessoaDestino.push(monteDestino);
+    }
+    const qtdReal = Math.min(quantidade, monteOrigem.qtd);
+    monteDestino.qtd += qtdReal;
+    monteOrigem.qtd -= qtdReal;
+}
+
+function dePomarParaPessoa(destino, tipo, quantidade) {
+    const pessoa = estoque[destino];
+    for (let i = 0; i < pessoa.length; i++) {
+        const monte = pessoa[i];
+        if (monte.tipo === tipo) {
+            monte.qtd += quantidade;
+            return;
+        }
+    }
+    const novoMonte = { 'tipo': tipo, 'qtd': Math.max(quantidade, 0) };
+    pessoa.push(novoMonte);
+}
+
+function dePessoaParaPomar(origem, tipo, quantidade) {
+    const pessoa = estoque[origem];
+    for (let i = 0; i < pessoa.length; i++) {
+        const monte = pessoa[i];
+        if (monte.tipo === tipo) {
+            monte.qtd -= Math.min(quantidade, monte.qtd);
+        }
     }
 }
 
