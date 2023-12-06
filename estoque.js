04086b25f511a2a95aaa3d8f4bc490cfdcea1a1b
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-05 22:02:30 -0300

    corrige escrita na funcao

--- estoque.js
+++ estoque.js
@@ -1,16 +1,16 @@
 let estoque ={
     'joao':[
         {'tipo': 'maca', 'qtd': 1},
-        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'pera', 'qtd': 2},
 ],
     'maria':[
         {'tipo': 'maca', 'qtd': 1},
-        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'banana', 'qtd': 2},
 ],
 };
 
 function getEstoque(){
-    return structedClone(estoque);
+    return structuredClone(estoque);
 }
 
 export {getEstoque};
